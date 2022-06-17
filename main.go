e355ef660adb28174a82f99887431ca59c35d1b1
zesagata <zezagata17@gmail.com>
2022-06-17 11:53:48 +0700

    remove wait

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ var (
 	LeaderKey   = "leader-election-key"
 	WaitTime    = 1 * time.Second
 	WorkTime    = 5 * time.Second
-	StandByTime = 10 * time.Second
+	StandByTime = 5 * time.Second
 )
 
 func main() {
@@ -38,14 +38,13 @@ func main() {
 	election := concurrency.NewElection(session, LeaderKey)
 
 	for {
-		isLeader := DoElection(election, *name)
+		isLeader := DoOrWaitElection(election, *name)
 		if isLeader {
 			fmt.Println("Im a leader ", *name)
 			DoSomeWork(election)
 		} else {
 			fmt.Println("Im a follower, standby...")
-			time.Sleep(StandByTime)
-			//WaitForReElection(election)
+			//time.Sleep(StandByTime)
 		}
 	}
 }
@@ -67,47 +66,19 @@ func DoSomeWork(election *concurrency.Election) {
 
 	}()
 	for {
-		fmt.Println("Do some work")
+		leaderKey, _ := election.Leader(context.Background())
+		fmt.Println(leaderKey)
+		fmt.Println("Do some work , ", election.Key())
 		time.Sleep(WorkTime)
 	}
 }
 
-// TODO implement wait for re election
-//func WaitForReElection(election *concurrency.Election) {
-//	observe := election.Observe(context.Background())
-//	for s := range observe {
-//		fmt.Println("Changee")
-//		fmt.Printf("%+v\n", s)
-//		break
-//	}
-//	fmt.Println("Leader is change")
-//	return
-//}
-
-func DoElection(election *concurrency.Election, name string) bool {
+func DoOrWaitElection(election *concurrency.Election, name string) bool {
 	ctx, _ := context.WithTimeout(context.Background(), WaitTime)
 	err := election.Campaign(ctx, name)
 
 	if err != nil && err == context.DeadlineExceeded {
 		return false
 	}
-	fmt.Println(err)
 	return true
 }
-
-//func WatchExpiredLease(cli *clientv3.Client) <- chan *clientv3.Event {
-//	events := make(chan *clientv3.Event)
-//
-//	wch := cli.Watch(context.Background(), LeaderKey)
-//
-//	go fun() {
-//		defer close(events)
-//		for wResp := range wch {
-//			for _, ev := range wResp.Events {
-//				expired, err :=
-//			}
-//		}
-//	}
-//
-//	return events
-//}
